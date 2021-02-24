3f73f39f090a8a99795f72ea9bbe04809ed24af4
AdriaPadilla <adrian.padilla.m@gmail.com>
2021-02-24 20:32:34 +0100

    Update params.py

--- twitter_full_archive/params.py
+++ twitter_full_archive/params.py
@@ -2,7 +2,7 @@ query_params = {
     'start_time': '2021-02-04T00:00:00Z', # YOUR START DATE IN YYYY-MM-DDT00:00:00Z Format
     'end_time': '2021-02-10T00:00:00Z', # YOUR START DATE IN YYYY-MM-DDT00:00:00Z Format
     'tweet.fields': 'author_id,created_at,in_reply_to_user_id,possibly_sensitive,public_metrics,lang,source',
-    'max_results': 500,
+    'max_results': 500, #This is the max results admited by API endpoint
     'expansions': 'attachments.media_keys,author_id',
     'media.fields': 'duration_ms,media_key,url,type,public_metrics',
     'user.fields': 'username',
