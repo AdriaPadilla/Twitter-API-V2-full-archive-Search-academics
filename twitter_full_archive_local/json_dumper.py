e9b37ea963d10d9fd392a08d94ff9cabba91c342
AdriaPadilla <adrian.padilla.m@gmail.com>
2021-11-16 20:25:35 +0100

    Update json_dumper.py

--- twitter_full_archive_local/json_dumper.py
+++ twitter_full_archive_local/json_dumper.py
@@ -3,16 +3,22 @@ import json
 
 from datetime import datetime
 
-output_folder = "output/"
+
 actual_time = datetime.now()
 
-def save_data(json_response, loop_counter, pharse, filename):
+def save_data(json_response, loop_counter, pharse, filename, capture_name):
+    output_folder = f"{capture_name}/api_responses/"
+    json_parsed = json_response.json()
+
+    data = json_parsed["data"]
+
+    n_tweets = len(data)
+    last = data[-1]
+    last_date = last["created_at"]
 
     counter_n = str(loop_counter)
 
     # THIS IS ONLY IF YOU WANT TO SAVE JSON PURE DATA. CREATES ONE JSON FILE FOR EACH LOOP.
-
-    json_parsed = json_response.json()
 
     try:
         if not os.path.exists(output_folder):
@@ -20,10 +26,7 @@ def save_data(json_response, loop_counter, pharse, filename):
         with open((output_folder + f"{filename}__loop-{counter_n}.json"), 'w', encoding='utf-8') as f:
             json.dump(json_parsed, f, ensure_ascii=False, indent=4)
             print(f"Loop {loop_counter} --> Dumped to JSON FILE {filename}")
+        return n_tweets, last_date
     except IndexError:
         print("ERROR")
         pass
-
-
-
-
