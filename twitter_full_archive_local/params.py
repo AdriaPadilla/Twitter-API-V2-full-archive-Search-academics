426303058fd6ede4a5774d1a715a0cd9836a9d72
Adrià Padilla <adrian.padilla.m@gmail.com>
2022-11-30 12:12:54 +0100

    Update params.py

--- twitter_full_archive_local/params.py
+++ twitter_full_archive_local/params.py
@@ -3,7 +3,7 @@ def parameters(start_date, end_date, start_time, end_time, hashtag):
         'start_time': f'{start_date}T{start_time}Z', # API DATE/HOUR FORMAT = YYYY-MM-DDTHH:MM:SSZ
         'end_time': f'{end_date}T{end_time}Z',
         'tweet.fields': 'author_id,created_at,in_reply_to_user_id,possibly_sensitive,public_metrics,lang,source,entities,geo,reply_settings,conversation_id',
-        'max_results': 500,
+        'max_results': 490,
         'expansions': 'attachments.media_keys,author_id,geo.place_id,referenced_tweets.id',
         'media.fields': 'duration_ms,media_key,url,type,public_metrics',
         'user.fields': 'username',
